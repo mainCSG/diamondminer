431f55d131e230d905cee64d91036097f6bf923c
Andrija <andrijapaurevic@gmail.com>
2024-07-19 15:59:52 -0400

    adjusted temp calculations

--- diamondminer/miner.py
+++ diamondminer/miner.py
@@ -57,14 +57,14 @@ class CoulombDiamond:
         return self.gate_capacitance() / self.gamma()
 
     def drain_capacitance(self) -> float:
-        return self.gate_capacitance() * (1- self.beta()) / self.beta() 
+        return self.gate_capacitance() * ((1- self.beta()) / self.beta() )
     
     def gate_capacitance(self) -> float:
         return self.e / self.addition_voltage()
     
     def dot_area(self) -> float:
         # Assumes parallel plate capicitaor geometry
-        dot_area = self.oxide_thickness * self.total_capacitance() / (self.eps0 * self.epsR)
+        dot_area = (self.oxide_thickness * self.total_capacitance()) / (self.eps0 * self.epsR)
         return dot_area
 
     def dot_radius(self) -> float:
@@ -291,6 +291,7 @@ class Miner:
             self, 
             diamonds: list[CoulombDiamond], 
             ohmic_value: float,
+            temperature_guess: float = 1,
             axes: Optional[plt.Axes] = None) -> list[float]:
         
         temperatures = []
@@ -301,7 +302,7 @@ class Miner:
             left_Vg, left_Vsd = left_diamond.top_vertex
             right_Vg, right_Vsd = right_diamond.top_vertex
             average_lever_arm = (left_diamond.lever_arm() + right_diamond.lever_arm()) / 2
-            
+
             # Filter out the coulomb oscillation between two diamonds
             gate_mask = np.where((self.gate_data >= left_Vg) * (self.gate_data <= right_Vg))[0]
             ohmic_index = (np.abs(self.ohmic_data - ohmic_value)).argmin()
@@ -310,9 +311,13 @@ class Miner:
             oscillation = self.current_data[ohmic_index, gate_mask]
 
             # Fit data to Coulomb peak theoretical formula
-            guess = [oscillation.min(), oscillation.max(), np.average(P_data_filtered), 1]
+            guess = [oscillation.min(), oscillation.max(), np.average(P_data_filtered), temperature_guess]
             (a,b,V0,Te), coeffs_cov = sp.optimize.curve_fit(
-            lambda V, a, b, V0, Te: self.coulomb_peak(V, average_lever_arm, a, b, V0, Te), P_data_filtered, oscillation, p0=guess
+                lambda V, a, b, V0, Te: self.coulomb_peak(V, average_lever_arm, a, b, V0, Te), 
+                P_data_filtered, 
+                oscillation, 
+                p0=guess,
+                bounds=([-np.inf, -np.inf, -np.inf, -np.inf],[np.inf,np.inf,np.inf,np.inf])
             )
 
             # Plot results
@@ -325,20 +330,20 @@ class Miner:
 
             temperatures.append(Te)
         temperatures = np.array(temperatures)
-        T_avg = np.average(temperatures)
+        T_avg = np.average(np.abs(temperatures))
         T_stddev = np.std(temperatures)
         if axes is not None:
             axes.annotate(
-                rf'T = ${round(T_avg,3)}$ K $\pm\ {round(T_stddev/np.sqrt(len(temperatures)), 3)}$ K',
-                (0.35,0.9),
+                rf'$T_e$ = ${round(T_avg,3)}$ K $\pm\ {round(T_stddev/np.sqrt(len(temperatures)), 3)}$ K',
+                (0.35,1.05),
                 xycoords='axes fraction',
-                size=8
+                size=10
                 )
         return temperatures
 
     def coulomb_peak(self, V, alpha, a, b, V0, Te):
         kB = 8.6173303e-5 # eV / K
-        return a + b * (np.cosh(alpha * (V0 - V) / (2 * kB * Te)))**-2
+        return a + b * (np.cosh(alpha * (V0 - V) / (2 * kB * Te)))**(-2)
 
     def get_statistics(self, diamonds: Optional[list[CoulombDiamond]] = None) -> dict:
         if diamonds is None:
@@ -355,14 +360,14 @@ class Miner:
             'dot_radius',
         ]
         methods_print = {
-            'lever_arm': lambda mu, std: f"Average Lever Arm (\u03B1) : {mu:.5f} (eV/V) \u00b1 {std:.5f} (eV/V)",
-            'addition_voltage' : lambda mu, std: f"Average Addition Voltage: {mu:.5f} (V) \u00b1 {std:.5f} (V)",
-            'charging_voltage': lambda mu, std: f"Average Charging Voltage: {mu:.5f} (V) \u00b1 {std:.5f} (V)",
-            'total_capacitance': lambda mu, std: f"Average Total Capacitance: {1e18 * mu:.5f} (aF) \u00b1 {1e18 * std:.5f} (aF)",
-            'gate_capacitance': lambda mu, std: f"Average Gate Capacitance: {1e18 * mu:.5f} (aF) \u00b1 {1e18 * std:.5f} (aF)",
-            'source_capacitance': lambda mu, std: f"Average Source Capacitance: {1e18 * mu:.5f} (aF) \u00b1 {1e18 * std:.5f} (aF)",
-            'drain_capacitance': lambda mu, std: f"Average Drain Capacitance: {1e18 * mu:.5f} (aF) \u00b1 {1e18 * std:.5f} (aF)",
-            'dot_radius': lambda mu, std: f"Average Dot Radius: {1e9 * mu:.5f} (nm) \u00b1 {1e9 * std:.5f} (nm)",
+            'lever_arm': lambda mu, std: f"Average Lever Arm (\u03B1) : {mu:.5f} eV/V \u00b1 {std:.5f} eV/V",
+            'addition_voltage' : lambda mu, std: f"Average Addition Voltage: {1e3 * mu:.5f} mV \u00b1 {1e3 *std:.5f} mV",
+            'charging_voltage': lambda mu, std: f"Average Charging Voltage: {1e3 *mu:.5f} mV \u00b1 {1e3 *std:.5f} mV",
+            'total_capacitance': lambda mu, std: f"Average Total Capacitance: {1e18 * mu:.5f} aF \u00b1 {1e18 * std:.5f} aF",
+            'gate_capacitance': lambda mu, std: f"Average Gate Capacitance: {1e18 * mu:.5f} aF \u00b1 {1e18 * std:.5f} aF",
+            'source_capacitance': lambda mu, std: f"Average Source Capacitance: {1e18 * mu:.5f} aF \u00b1 {1e18 * std:.5f} aF",
+            'drain_capacitance': lambda mu, std: f"Average Drain Capacitance: {1e18 * mu:.5f} aF \u00b1 {1e18 * std:.5f} aF",
+            'dot_radius': lambda mu, std: f"Average Dot Radius: {1e9 * mu:.5f} nm \u00b1 {1e9 * std:.5f} nm",
         }
 
 
